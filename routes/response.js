87fac1aa2bea96b57e6cdb58e5983526a75e2514
Ammar Khan <ammar.hayder@gmail.com>
2017-04-10 11:04:49 +0000

    Add lodash utils

--- routes/response.js
+++ routes/response.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
 /* api for form get post data and response the same */
 router.post('/', function(req, res, next) {
-  res.send(req.body);
+    // var responseArray = [req.body];
+    _.forIn(req.body, function(value, key){
+        req.body[key] = _.capitalize(value);
+    });
+    res.send(_.set(req.body, 'age', 40) );
 });
 
 module.exports = router;
